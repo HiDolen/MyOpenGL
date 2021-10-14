91a051fe88a0eeab130017c9d768021a835a069b
HiDolen <820859278@qq.com>
2021-10-14 09:35:05 +0800

    初始化Camera头文件

--- src/Camera.cpp
+++ src/Camera.cpp
@@ -1,21 +1,38 @@
 #ifndef Camera_h
 #define Camera_h
 
-#include <iostream>
-#include <math.h>
-#include <algorithm>
-//GLEW
+#include <vector>
 #define GLEW_STATIC
 #include <GL/glew.h>
-//GLFW
-#include <GLFW/glfw3.h>
-
-#include "Shader.h"
-
-#include <SOIL2/SOIL2.h>
 
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
-#include <glm/gtc/type_ptr.hpp>
+
+const GLfloat YAW = -90.0f;
+const GLfloat PITCH = 0.0f;
+const GLfloat ZOOM = 45.0f;
+
+class Camera
+{
+public:
+    Camera(glm::vec3 position = glm::vec3(0.0f, 0.0f, 0.0f),
+           glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f),
+           GLfloat yaw = YAW,
+           GLfloat pitch = PITCH) : front(glm::vec3(0.0f, 0.0f, -1.0f)),
+                                    zoom(ZOOM)
+    {
+    }
+
+private:
+    GLfloat yaw;
+    GLfloat pitch;
+    GLfloat zoom;
+
+    glm::vec3 position;
+    glm::vec3 front;
+    glm::vec3 right;
+    glm::vec3 up;
+    glm::vec3 worldUp;
+};
 
 #endif
